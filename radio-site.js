76301982c6b1689369f1526947a73d8e83f249a8
Chabma <chabma@gmail.com>
2016-02-28 15:56:22 -0500

    PhP Test 10
    Fixed a silly error in javascript code

--- radio-site.js
+++ radio-site.js
@@ -157,6 +157,7 @@ function play_song(show_num, episode_num, song_num) {
     xmlhttp.onreadystatechange = function() {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
             console.log(xmlhttp.responseText);
+            var response = "";
             source.src = response.match(/<body>([\s\S]*)<\/body>/);
             console.log(source.src);
             console.log("response: " + response);
