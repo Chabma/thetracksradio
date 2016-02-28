6d25e2cc99aa284362e95f4cb72f2a0766342c0d
Chabma <chabma@gmail.com>
2016-02-28 15:53:26 -0500

    PHP Test 9

--- radio-site.js
+++ radio-site.js
@@ -157,7 +157,7 @@ function play_song(show_num, episode_num, song_num) {
     xmlhttp.onreadystatechange = function() {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
             console.log(xmlhttp.responseText);
-            source.src = response.match(/echo\('(.*)'\)/g);
+            source.src = response.match(/<body>([\s\S]*)<\/body>/);
             console.log(source.src);
             console.log("response: " + response);
             audio.currentTime = 0; 
