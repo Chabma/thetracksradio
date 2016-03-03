0c6e4b739ca157dcecb9ab7224672bbb637227cd
Chabma <chabma@gmail.com>
2016-03-03 04:08:13 -0500

    corrected typo

--- getcontent.php
+++ getcontent.php
@@ -29,7 +29,7 @@
             echo(':');
             echo(floor(($row['Duration']%360)/60));
             echo(':');
-            echo(floor($row['Duration'])%60) );
+            echo(floor($row['Duration']%60));
             echo('</p></li>');
         }
         mysqli_close($con);
