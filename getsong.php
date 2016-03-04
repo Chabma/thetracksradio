a707b9e40bb1d4b0a624c1a3d1b19f0dd7c6c346
Chabma <chabma@gmail.com>
2016-03-03 22:04:12 -0500

    Revert "Debugging"
    This reverts commit 13542f721bffa0b7444ffae878e05b21f8412cc6.

--- getsong.php
+++ getsong.php
@@ -107,6 +107,8 @@
             while($row = mysqli_fetch_array($result)) {
                 echo($row['Title']);
             }
+            
+            
             mysqli_close($con);
         ?>
         </div>
