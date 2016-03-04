c43e3a222e557f5d13cb8faac5627dcf790d67bd
Chabma <chabma@gmail.com>
2016-03-03 22:35:50 -0500

    Revert "Revert "Revert "Moved php request headers"""
    This reverts commit 6856449c0e3adc95ef78f1cd190b36682e2b9590.

--- radio-site.js
+++ radio-site.js
@@ -202,6 +202,7 @@ function get_background(number) {
         // code for IE6, IE5
         xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
     }
+    xmlhttp.setRequestHeader("referer", "http://thetracksradio.com/");
     xmlhttp.onreadystatechange = function() {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
             response = xmlhttp.responseText;
@@ -215,7 +216,6 @@ function get_background(number) {
         }
     };
     xmlhttp.open("GET","./getbackground.php?a="+number+",true");
-    xmlhttp.setRequestHeader("referer", "http://thetracksradio.com/");
     xmlhttp.send();
 }
 
@@ -266,6 +266,7 @@ function play_song(show_num, episode_num, song_num, _callback) {
         // code for IE6, IE5
         xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
     }
+    xmlhttp.setRequestHeader("referer", "http://thetracksradio.com/");
     xmlhttp.onreadystatechange = function() {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
             response = xmlhttp.responseText;
@@ -288,7 +289,6 @@ function play_song(show_num, episode_num, song_num, _callback) {
         }
     };
     xmlhttp.open("GET","./getsong.php?a="+show_num+"&b="+episode_num+"&c="+song_num+"",true);
-    xmlhttp.setRequestHeader("referer", "http://thetracksradio.com/");
     xmlhttp.send();
 }
 
