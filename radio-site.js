73b5565b074761cc59d29f776277d32f304c0a63
Chabma <chabma@gmail.com>
2016-03-03 22:39:42 -0500

    headers""""
    This reverts commit faec7d95d53ff17fef975fa0df9ec17d0fa1c271.

--- radio-site.js
+++ radio-site.js
@@ -215,6 +215,7 @@ function get_background(number) {
         }
     };
     xmlhttp.open("GET","./getbackground.php?a="+number+",true");
+    xmlhttp.setRequestHeader("referer", "http://thetracksradio.com/");
     xmlhttp.send();
 }
 
@@ -287,6 +288,7 @@ function play_song(show_num, episode_num, song_num, _callback) {
         }
     };
     xmlhttp.open("GET","./getsong.php?a="+show_num+"&b="+episode_num+"&c="+song_num+"",true);
+    xmlhttp.setRequestHeader("referer", "http://thetracksradio.com/");
     xmlhttp.send();
 }
 
