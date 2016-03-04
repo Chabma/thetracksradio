1881df9adb914d596585f57480adbdee93f391bb
Chabma <chabma@gmail.com>
2016-03-03 22:04:12 -0500

    Revert "Finally things are working again"
    This reverts commit 196d97ffab876ce745bea0494d900d56e19ab2ff.

--- getsong.php
+++ getsong.php
@@ -18,65 +18,27 @@
                 die('Could not connect: ' . mysqli_error($con));
             }
             
-            if(!function_exists('el_crypto_hmacSHA1')){
-    /**
-    * Calculate the HMAC SHA1 hash of a string.
-    *
-    * @param string $key The key to hash against
-    * @param string $data The data to hash
-    * @param int $blocksize Optional blocksize
-    * @return string HMAC SHA1
-    */
-    function el_crypto_hmacSHA1($key, $data, $blocksize = 64) {
-        if (strlen($key) > $blocksize) $key = pack('H*', sha1($key));
-        $key = str_pad($key, $blocksize, chr(0x00));
-        $ipad = str_repeat(chr(0x36), $blocksize);
-        $opad = str_repeat(chr(0x5c), $blocksize);
-        $hmac = pack( 'H*', sha1(
-        ($key ^ $opad) . pack( 'H*', sha1(
-          ($key ^ $ipad) . $data
-        ))
-      ));
-        return base64_encode($hmac);
-    }
-  }
+            function getTemporaryUrl($key, $secret, $bucket, $path, $expirySeconds = 30){
+                $expiry = time() + $expirySeconds;
+            
+                // Format the string to be signed
+                $string = sprintf("GET\n\n\n%s\n/%s/%s", $expiry, $bucket, $path);
 
-  if(!function_exists('el_s3_getTemporaryLink')){
-    /**
-    * Create temporary URLs to your protected Amazon S3 files.
-    *
-    * @param string $accessKey Your Amazon S3 access key
-    * @param string $secretKey Your Amazon S3 secret key
-    * @param string $bucket The bucket (bucket.s3.amazonaws.com)
-    * @param string $path The target file path
-    * @param int $expires In minutes
-    * @return string Temporary Amazon S3 URL
-    * @see http://awsdocs.s3.amazonaws.com/S3/20060301/s3-dg-20060301.pdf
-    */
-    
-    function el_s3_getTemporaryLink($accessKey, $secretKey, $bucket, $path, $expires = 5) {
-      // Calculate expiry time
-      $expires = time() + intval(floatval($expires) * 60);
-      // Fix the path; encode and sanitize
-      $path = str_replace('%2F', '/', rawurlencode($path = ltrim($path, '/')));
-      // Path for signature starts with the bucket
-      $signpath = '/'. $bucket .'/'. $path;
-      // S3 friendly string to sign
-      $signsz = implode("\n", $pieces = array('GET', null, null, $expires, $signpath));
-      // Calculate the hash
-      $signature = el_crypto_hmacSHA1($secretKey, $signsz);
-      // Glue the URL ...
-      $url = sprintf('http://%s.s3.amazonaws.com/%s', $bucket, $path);
-      // ... to the query string ...
-      $qs = http_build_query($pieces = array(
-        'AWSAccessKeyId' => $accessKey,
-        'Expires' => $expires,
-        'Signature' => $signature,
-      ));
-      // ... and return the URL!
-      return $url.'?'.$qs;
-    }
-  }       
+                // Generate an HMAC-SHA1 signature for it, simpler times.
+                $signature = base64_encode(hash_hmac('sha1', $string, $secret, true));
+
+                // Create the final URL
+                return sprintf(
+                "https://%s.s3.amazonaws.com/%s?%s",
+                $bucket,
+                $path,
+                http_build_query([
+                    'AWSAccessKeyId' => $key,
+                    'Expires' => $expiry,
+                    'Signature' => $signature
+                ])
+                );
+            }       
             
             
 
@@ -87,7 +49,21 @@
             $result = mysqli_query($con,$sql);
             while($row = mysqli_fetch_array($result)) {
                 //echo(getTemporaryUrl("AKIAJPM5BXNE3ATMIBJQ", "clxpOdDJNOE7y+OxME4Mbx0Leex/aV0JtU+onfvX","thetracksradio-mp3s", $row['Path'],100));
-                echo(el_s3_getTemporaryLink("AKIAJPM5BXNE3ATMIBJQ", "clxpOdDJNOE7y+OxME4Mbx0Leex/aV0JtU+onfvX", "thetracksradio-mp3s", $row['Path'], 5));
+                
+                $s3Client = new Aws\S3\S3Client([
+                    'region'  => 'us-standard',
+                    'version' => '2016-02-26',
+                ]);
+                
+                $cmd = $s3Client->getCommand('GetObject', [
+                    'Bucket' => 'thetracksradio-mp3s',
+                    'Key'    => $row['Path'] 
+                ]); 
+                
+                $request = $s3Client->createPresignedRequest($cmd, '+2 minutes');
+                
+                $presignedUrl = (string) $request->getUri();
+                echo($url);
             }
             mysqli_close($con);
         ?>
