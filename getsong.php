c95c560612a1b692ac8dbaf8b459f0bf43c9734d
Chabma <chabma@gmail.com>
2016-03-08 04:45:18 -0500

    Temporary way to show all song info

--- getsong.php
+++ getsong.php
@@ -206,7 +206,7 @@
             $sql="SELECT * FROM Songs WHERE Show_Id = '".$a."' AND Episode_Id = '".$b."' AND Song_Num = '".$c."'";
             $result = mysqli_query($con,$sql);
             while($row = mysqli_fetch_array($result)) {
-                echo(" / ");
+                echo(" ~ ");
                 echo($row['Artist']);
                 echo(" - ");
                 echo($row['Album']);
