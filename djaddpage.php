489b6a5e38a290a91dc234e3bc864546000e7b88
Chabma <chabma@gmail.com>
2016-10-11 14:27:32 -0400

    Added dj page framework
    Based on previous pages

---
+++ djaddpage.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+    <body>
+        <?php
+        $q = intval($_GET['q']);
+        $con = mysqli_connect('eastone.c3y2bcgdn85r.us-east-1.rds.amazonaws.com','cam','fogter01','thetracksradio_database');
+        if (!$con) {
+            die('Could not connect: ' . mysqli_error($con));
+        }
+        mysqli_select_db($con,"thetracksradio_database");
+        $sql="SELECT * FROM Episodes WHERE Show_Id = '".$q."' ORDER BY Release_Date DESC";
+        $result = mysqli_query($con,$sql); 
+        while($row = mysqli_fetch_array($result)) {
+            echo('<td onclick= "');
+            echo($row['Function']);
+            echo($row['Show_Id']);
+            echo(',');
+            echo($row['Episode_Id']);
+            echo(')">');
+            echo('<img class="episode_image" src="');
+            echo($row['IMG_Location']);
+            echo('"><img class="overlay" src="images/button-images/overlay.png"><div class="episodes-paragraph"><h4>');
+            echo($row['Title']);
+            echo('</h4></br><p>');
+            if( $row['Function'] == 'get_count(' ) {
+                echo('Duration: ');
+                echo(floor($row['Duration']/3600));
+                echo(':');
+                echo(sprintf("%02d", floor(($row['Duration']%3600)/60)));
+                echo(':');
+                echo(sprintf("%02d", floor($row['Duration']%60)));
+            }
+            echo('</p></td>');
+        }
+        mysqli_close($con);
+        ?>
+    </body>
+</html>
---
+++ djlogin.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+    <body>
+        <?php
+        $con = mysqli_connect('eastone.c3y2bcgdn85r.us-east-1.rds.amazonaws.com','cam','fogter01','thetracksradio_database');
+        if (!$con) {
+            die('Could not connect: ' . mysqli_error($con));
+        }
+        $show_titles = ["","","Twix God","Jams","SoccerShow","John's Thing","Luke's Comic"];
+        
+        mysqli_select_db($con,"thetracksradio_database");
+        $sql="SELECT * FROM Episodes ORDER BY Release_Date DESC";
+        $result = mysqli_query($con,$sql);  
+        while($row = mysqli_fetch_array($result)) {
+            echo('<td onclick= "');
+            echo($row['Function']);
+            echo($row['Show_Id']);
+            echo(',');
+            echo($row['Episode_Id']);
+            echo(')">');
+            echo('<img class="episode_image" src="');
+            echo($row['IMG_Location']);
+            echo('"><img class="overlay" src="images/button-images/overlay.png"><div class="episodes-paragraph"><h4>From ');
+            echo($show_titles[$row['Show_Id']]);
+            echo(': ');
+            echo($row['Title']);
+            echo('</h4></br><p>');
+            if($row['Function'] == 'get_count('){
+                echo('Duration: ');
+                echo(floor($row['Duration']/3600));
+                echo(':');
+                echo(sprintf("%02d", floor(($row['Duration']%3600)/60)));
+                echo(':');
+                echo(sprintf("%02d", floor($row['Duration']%60)));
+            }   
+            echo('</p></td>');
+        }
+        mysqli_close($con);
+        ?>
+    </body>
+</html>
