643279a36560280efd665e2d35929daa2e6006c1
Chabma <chabma@gmail.com>
2016-10-21 19:55:30 -0400

    debugging login a bit

--- djlogin.php
+++ djlogin.php
@@ -1,6 +1,7 @@
 <?php
    include("config.php");
    session_start();
+    echo("login attempted")
    
    if($_SERVER["REQUEST_METHOD"] == "POST") {
       // username and password sent from form 
